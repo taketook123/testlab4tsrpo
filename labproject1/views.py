7ca3b786446230446d7eea6ff4354d5ce0ce3ea5
taketook123 <taketook26@gmail.com>
2023-12-06 07:46:31 -0500

    Adding some jwt exceptions

--- labproject1/views.py
+++ labproject1/views.py
@@ -14,6 +14,36 @@ with app.app_context():
     db.create_all()
     db.session.commit()
 
+
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_payload):
+   return (
+       jsonify({"message": "The token has expired.", "error": "token_expired"}),
+       401,
+   )
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+   return (
+       jsonify(
+           {"message": "Signature verification failed.", "error": "invalid_token"}
+       ),
+       401,
+   )
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+   return (
+       jsonify(
+           {
+               "description": "Request does not contain an access token.",
+               "error": "authorization_required",
+           }
+       ),
+       401,
+   )
+
+
 @app.route('/healthcheck')
 def healthcheck_page():
     cur_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
