059222988dbfe3390c13e8e6681595aff57adbae
taketook123 <taketook26@gmail.com>
2023-12-06 08:30:19 -0500

    Add some conf fixes 2

--- labproject1/__init__.py
+++ labproject1/__init__.py
@@ -8,7 +8,7 @@ import os
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:SomePassword@localhost/test"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://lab4testserv_user:JNTiH7jPXqzWIP5EA3CXkcnylV2CVkwt@dpg-clo65mv5felc73a2tqh0-a/lab4testserv"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab4testserv_user:JNTiH7jPXqzWIP5EA3CXkcnylV2CVkwt@dpg-clo65mv5felc73a2tqh0-a/lab4testserv"
 app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
 app.config['JWT_ALGORITHM'] = "HS256"
 db = SQLAlchemy(app)
