559d379b54b3a97dd76187da211d2f23012648ec
taketook123 <taketook26@gmail.com>
2023-12-06 08:01:36 -0500

    fix some entities

--- labproject1/serializers.py
+++ labproject1/serializers.py
@@ -2,6 +2,7 @@ from marshmallow import Schema, fields, validate, ValidationError
 
 class UserSchema(Schema):
     name = fields.String(required=True)
+    password = fields.String(required=True)
 
 class CategorySchema(Schema):
     name = fields.String(required=True)
