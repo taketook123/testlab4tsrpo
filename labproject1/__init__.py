18d899d18cf2556e6d4f51e1f4971bb017b3c25e
taketook123 <taketook26@gmail.com>
2023-12-06 06:47:01 -0500

    Update serializers and init

--- labproject1/__init__.py
+++ labproject1/__init__.py
@@ -2,13 +2,18 @@ from flask import Flask, request, jsonify
 import uuid
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_restful import Api
+import os
 
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:SomePassword@localhost/test"
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://testlab3db_q1ss_user:UMK3TtGSlxbwmixK5QPVAN20CQVsW74x@dpg-cles1ac15k1s73f5kshg-a/testlab3db_q1ss"
+app.config['JWT_SECRET_KEY'] = os.environ('JWT_SECRET_KEY')
+app.config['JWT_ALGORITHM'] = "HS256"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+api = Api(app)
 
 import labproject1.views
 import labproject1.models
--- labproject1/serializers.py
+++ labproject1/serializers.py
@@ -8,10 +8,10 @@ class CategorySchema(Schema):
 
 class RecordSchema(Schema):
     category_id = fields.Integer(required=True, validate=validate.Range(min=0))
-    user_id = fields.Integer(required=True, validate=validate.Range(min=0))
+    #user_id = fields.Integer(required=True, validate=validate.Range(min=0))
     sum = fields.Float(required=True, validate=validate.Range(min=0.0))
 
 class UserCategorySchema(Schema):
     name = fields.String(required=True)
-    user_id = fields.Integer(required=True, validate=validate.Range(min=0))
+    #user_id = fields.Integer(required=True, validate=validate.Range(min=0))
     is_public = fields.Boolean(required=True)
